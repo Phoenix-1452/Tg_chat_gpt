d741231cb33576cac20c8e77448b7828b10c7ec3
Phoenix-1452 <sadovodov23092002@mail.ru>
2023-07-06 12:18:10 +0300

    add

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,7 +10,7 @@ openai.api_key = os.getenv("API_CHAT_GPT_KEY")
 bot = Bot(TELEGRAM_KEY)
 dp = Dispatcher(bot)
 
-msg = {}
+history = {}
 
 
 @dp.message_handler(commands=['start'])
@@ -20,32 +20,32 @@ async def start_msg(message: types.Message):
 
 @dp.message_handler(commands=['history'])
 async def gpt_history(message: types.Message):
-    await message.answer(str(msg))
+    await message.answer(str(history))
 
 
 @dp.message_handler(commands=['clear'])
 async def gpt_history(message: types.Message):
-    msg.clear()
+    history.clear()
     await message.answer("History deleted.")
 
 
 @dp.message_handler()
 async def gpt_answer(message: types.Message):
     flood = await message.answer('Generating...')
-    if not (message.from_user.id in msg):
-        msg[message.from_user.id] = []
-    msg[message.from_user.id].append({'role': 'user', 'content': message.text})
+    if not (message.from_user.id in history):
+        history[message.from_user.id] = []
+    history[message.from_user.id].append({'role': 'user', 'content': message.text})
 
     completion = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=msg[message.from_user.id],
+        messages=history[message.from_user.id],
         temperature=0.5
     )
 
     answer = completion['choices'][0]['message']['content']
     await message.reply(answer)
     await flood.delete()
-    msg[message.from_user.id].append({'role': 'assistant', 'content': answer})
+    history[message.from_user.id].append({'role': 'assistant', 'content': answer})
 
 
 if __name__ == "__main__":
