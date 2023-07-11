3e2b4b0f10b7b8a708e72fd041fea32c2933f40e
mordegira <kirill.smirnov.spb@gmail.com>
2023-07-11 12:59:00 +0300

    добавил функцию регистрации

--- Db.py
+++ Db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Column, Integer, BigInteger, Float
+from sqlalchemy import create_engine, MetaData, Column, Integer, BigInteger, Float, DateTime
 from sqlalchemy.orm import Session, DeclarativeBase
 import Secret
 
@@ -6,7 +6,7 @@ class User_info:
     pass
         
 
-engine = create_engine(f"postgresql+psycopg2://postgres:{Secret.password}@localhost/Users")
+engine = create_engine(f"postgresql+psycopg2://postgres:KlimKva22@localhost/Users")
 session = Session(bind=engine)
 
 class Base(DeclarativeBase):
@@ -16,12 +16,14 @@ class User(Base):
 
     User_id = Column(Integer, primary_key=True)
     Telegram_id = Column(BigInteger)
-    Day_of_join = Column(Integer)
+    Day_of_join = Column(DateTime)
     Balance = Column(Float)
     Vip_status = Column(Integer)
 
 c1 = User(
-    Balance = 10)
+    Telegram_id = 3254513433,
+    Day_of_join = "2023-12-23 02:14:14",
+    Balance = 10.15)
 
 session.add(c1)
 session.commit()
